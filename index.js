c987f3c8874d1f0b99f2acd4eeb5683eb2970b23
lucas ivan ponce <lucasivanponce@gmail.com>
2023-04-05 14:05:07 -0300

    Solucionamos errores

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const productos=[
         "data":"White Gold",
         "precio": 7000.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado1.jpg",
+        "imagen": "./assets/img/joyas/destacado1.jpg",
     },
     {
         "id": "ID2",
@@ -13,7 +13,7 @@ const productos=[
         "data":"White Gold",
         "precio": 13500.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado2.jpg",
+        "imagen": "./assets/img/joyas/destacado2.jpg",
     },
     {
         "id": "ID3",
@@ -21,7 +21,7 @@ const productos=[
         "data":"Yellow Gold",
         "precio": 31500.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado3.jpg",
+        "imagen": "./assets/img/joyas/destacado3.jpg",
     },
     {
         "id": "ID4",
@@ -29,7 +29,7 @@ const productos=[
         "data":"Yellow Gold",
         "precio": 41000.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado4.jpg",
+        "imagen": "./assets/img/joyas/destacado4.jpg",
     },
     {
         "id": "ID5",
@@ -37,7 +37,7 @@ const productos=[
         "data":"White Gold",
         "precio": 6000.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado5.jpg",
+        "imagen": "./assets/img/joyas/destacado5.jpg",
     },
     {
         "id": "ID6",
@@ -45,7 +45,7 @@ const productos=[
         "data":"White Gold",
         "precio": 9000.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado6.jpg",
+        "imagen": "./assets/img/joyas/destacado6.jpg",
     },
     {
         "id": "ID7",
@@ -53,7 +53,7 @@ const productos=[
         "data":"Yellow Gold",
         "precio": 7500.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado7.jpg",
+        "imagen": "./assets/img/joyas/destacado7.jpg",
     },
     {
         "id": "ID8",
@@ -61,7 +61,7 @@ const productos=[
         "data":"Yellow Gold",
         "precio": 12500.00,
         "cantidad": 1,
-        "imagen": "../assets/img/joyas/destacado8.jpg",
+        "imagen": "./assets/img/joyas/destacado8.jpg",
     }
 ];
 const contenedorProductos = document.querySelector("#contenedor-productos");
